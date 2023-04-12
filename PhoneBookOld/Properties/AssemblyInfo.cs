87cb1392f71f7d8a6643c724ff6e93ec255cd4f8
Артём Грейс <artem_greis@mail.ru>
2023-04-12 22:54:48 +0300

    Полное переименование проекта

--- PhoneBookOld/Properties/AssemblyInfo.cs
+++ PhoneBookOld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("PhoneBook")]
+[assembly: AssemblyTitle("PhoneBookOld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PhoneBook")]
+[assembly: AssemblyProduct("PhoneBookOld")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
